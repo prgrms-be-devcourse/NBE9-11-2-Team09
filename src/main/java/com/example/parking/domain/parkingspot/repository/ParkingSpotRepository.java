c3f25675a4de7693622bf8cd6d494b7c4d933447
leeht62 <zdfxd3611@naver.com>
2026-04-10 17:08:52 +0900

    fix:parking-spot 도메인 분리#3

--- src/main/java/com/example/parking/domain/parkingspot/repository/ParkingSpotRepository.java
+++ src/main/java/com/example/parking/domain/parkingspot/repository/ParkingSpotRepository.java
@@ -1,9 +1,7 @@
-package com.example.parking.domain.parking.repository;
+package com.example.parking.domain.parkingspot.repository;
 
-import com.example.parking.domain.parking.entity.ParkingSpot;
-import com.example.parking.domain.parking.entity.SpotStatus;
+import com.example.parking.domain.parkingspot.entity.ParkingSpot;
 import org.springframework.data.jpa.repository.JpaRepository;
-import java.util.List;
 
 public interface ParkingSpotRepository extends JpaRepository<ParkingSpot, Long> {
 //    // 특정 주차장의 모든 자리 조회
