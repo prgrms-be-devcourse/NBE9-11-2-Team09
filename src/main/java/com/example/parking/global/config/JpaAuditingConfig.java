c0ea6c6bbec619b833263b2666afa142bc9630b7
minho <minho01@hs.ac.kr>
2026-04-13 09:13:04 +0900

    refactor: JPA Auditing 중복 설정 제거

--- src/main/java/com/example/parking/global/config/JpaAuditingConfig.java
+++
@@ -1,9 +0,0 @@
-package com.example.parking.global.config;
-
-import org.springframework.context.annotation.Configuration;
-import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
-
-@Configuration
-@EnableJpaAuditing
-public class JpaAuditingConfig {
-}
