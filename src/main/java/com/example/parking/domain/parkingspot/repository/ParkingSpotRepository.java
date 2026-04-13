d788fd7cafa4e2b7754dd7a65c7a88c1469b7b9e
leeht62 <zdfxd3611@naver.com>
2026-04-13 12:05:34 +0900

    Cus 02 (#19)
    * fix:parking-spot 도메인 분리#3
    
    * feat: parkingspot 초기 init 작성 #3
    
    * style: 주석 처리 및 코드개선

--- src/main/java/com/example/parking/domain/parkingspot/repository/ParkingSpotRepository.java
+++ src/main/java/com/example/parking/domain/parkingspot/repository/ParkingSpotRepository.java
@@ -1,12 +1,18 @@
 package com.example.parking.domain.parkingspot.repository;
 
 import com.example.parking.domain.parkingspot.entity.ParkingSpot;
+import jakarta.persistence.LockModeType;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Lock;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.Optional;
 
 public interface ParkingSpotRepository extends JpaRepository<ParkingSpot, Long> {
-//    // 특정 주차장의 모든 자리 조회
-//    List<ParkingSpot> findByParkingLotId(Long parkingLotId);
-//
-//    // 특정 주차장의 상태별(예: AVAILABLE) 자리 조회
-//    List<ParkingSpot> findByParkingLotIdAndStatus(Long parkingLotId, SpotStatus status);
+
+  @Lock(LockModeType.PESSIMISTIC_WRITE)
+  @Query("SELECT p FROM ParkingSpot p WHERE p.id = :spotId")
+  Optional<ParkingSpot> findByIdWithLock(@Param("spotId") Long spotId);
+
 }
