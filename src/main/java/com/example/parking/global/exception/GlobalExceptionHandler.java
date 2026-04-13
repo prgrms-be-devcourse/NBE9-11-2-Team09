a63f213afbeb0658bf817d6edd0ec21b913d788e
leeht62 <zdfxd3611@naver.com>
2026-04-13 16:06:07 +0900

    Cus 11 (#30)
    * fix:parking-spot 도메인 분리#3
    
    * feat: parkingspot 초기 init 작성 #3
    
    * style: 주석 처리 및 코드개선
    
    * feature: SSE 로직 추가 및 설정파일 제외 #11
    
    * fix:경로문제 해결
    
    * fix: 시큐리티 임시 문제 해결
    
    * fix: .gitignore 문제 해결
    
    * chore: application-local.yml 추적 제외 및 보안 조치 #22

--- src/main/java/com/example/parking/global/exception/GlobalExceptionHandler.java
+++ src/main/java/com/example/parking/global/exception/GlobalExceptionHandler.java
@@ -1,4 +1,58 @@
 package com.example.parking.global.exception;
 
+
+//@ControllerAdvice
 public class GlobalExceptionHandler {
+
+//  @ExceptionHandler(NoSuchElementException.class)
+//  @ResponseBody
+//  public RsData<Void> handleException(){
+//    return new RsData<Void>(
+//        "존재하지 않는 데이터입니다.",
+//        "404-1"
+//    );
+//  }
+//
+//  @ExceptionHandler(MethodArgumentNotValidException.class)
+//  @ResponseBody
+//  public RsData<Void> handleException(MethodArgumentNotValidException e) {
+//    String message = e.getBindingResult()
+//        .getAllErrors()
+//        .stream()
+//        .filter(error -> error instanceof FieldError)
+//        .map(error -> (FieldError) error)
+//        .map(error -> error.getField() + "-" + error.getCode() + "-" + error.getDefaultMessage())
+//        .sorted(Comparator.comparing(String::toString))
+//        .collect(Collectors.joining("\n"));
+//
+//    return new RsData<Void>(
+//        message,
+//        "400-1"
+//    );
+//  }
+//
+//  @ExceptionHandler(HttpMessageNotReadableException.class)
+//  @ResponseBody
+//  public RsData<Void> handleException(HttpMessageNotReadableException e) {
+//    return new RsData<Void>(
+//        "잘못된 형식의 요청 데이터입니다.",
+//        "400-2"
+//    );
+//  }
+//
+//  @ExceptionHandler(ServiceException.class)
+//  @ResponseBody
+//  public RsData<Void> handleException(ServiceException e) {
+//    return e.getRsData();
+//  }
+//
+//  @ExceptionHandler(HandlerMethodValidationException.class)
+//  @ResponseBody
+//  public RsData<Void> handleException(HandlerMethodValidationException e) {
+//    return new RsData<Void>(
+//        "잘못된 파라미터 요청입니다.",
+//        "400-4"
+//    );
+//  }
+
 }
