3b8b0b496374672e96733dc99d0e03b7cb879499
june <154238961+devjjune@users.noreply.github.com>
2026-04-13 17:50:27 +0900

    feat: 스프링 캐시 추가 (#36)

--- src/main/java/com/example/parking/ParkingApplication.java
+++ src/main/java/com/example/parking/ParkingApplication.java
@@ -2,9 +2,11 @@ package com.example.parking;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+@EnableCaching
 @EnableJpaAuditing
 @SpringBootApplication
 @ComponentScan(basePackages = "com.example.parking")
