1aa50bb059a8799323c422a95ade8c5f0136a930
sorbet0404 <151494073+sorbet0404@users.noreply.github.com>
2026-04-14 15:35:21 +0900

    Revert "Cus 11 (#60)"
    This reverts commit c26fd36cfb8dc73f61eac1ea5fd5a91a538041af.

--- src/main/java/com/example/parking/domain/parkingspot/entity/ParkingSpot.java
+++ src/main/java/com/example/parking/domain/parkingspot/entity/ParkingSpot.java
@@ -2,17 +2,10 @@ package com.example.parking.domain.parkingspot.entity;
 
 import com.example.parking.domain.parkingLot.entity.ParkingLot;
 import jakarta.persistence.*;
-import lombok.Builder;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-
-import java.time.LocalDateTime;
+import lombok.*;
 
 @Entity
-//@Table(name = "parking_spot", indexes = {
-//    @Index(name = "idx_parking_lot_status", columnList = "parking_lot_id, parking_spot_status")
-//})
-@Table(name = "parking_spot")
+@Table(name = "parking_spots")
 @Getter
 @NoArgsConstructor
 public class ParkingSpot {
@@ -37,9 +30,6 @@ public class ParkingSpot {
     @Column(name = "parking_spot_number", nullable = false, length = 50)
     private String number;
 
-    @Column(name = "reserved_at")
-    private LocalDateTime reservedAt;
-
     @Builder
     public ParkingSpot(ParkingLot parkingLot, String number, SpotType type) {
         this.parkingLot = parkingLot;
@@ -62,13 +52,5 @@ public class ParkingSpot {
             throw new IllegalStateException("이미 점유된 자리입니다.");
         }
         this.status = SpotStatus.OCCUPIED;
-        this.reservedAt = LocalDateTime.now();
     }
-
-    public void release() {
-        this.status = SpotStatus.AVAILABLE;
-        this.reservedAt = null;
-    }
-
-
 }
