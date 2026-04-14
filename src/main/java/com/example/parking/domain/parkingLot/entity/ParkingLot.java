3cd8e7a3d8893c3cefc3fb81300f120b3012f310
devjjune <jiyoon.hwang92@gmail.com>
2026-04-14 11:14:05 +0900

    chore: 주차장 필드 기본값 재설정

--- src/main/java/com/example/parking/domain/parkingLot/entity/ParkingLot.java
+++ src/main/java/com/example/parking/domain/parkingLot/entity/ParkingLot.java
@@ -15,9 +15,9 @@ import java.util.List;
 public class ParkingLot {
 
     // 기본값 세팅
-    private static final Integer DEFAULT_PRICE = 0;
-    private static final LocalTime DEFAULT_OPERATION_START_TIME = LocalTime.MIN; // 00:00
-    private static final LocalTime DEFAULT_OPERATION_END_TIME = LocalTime.of(23, 59);
+    private static final Integer DEFAULT_PRICE = 1000;
+    private static final LocalTime DEFAULT_OPERATION_START_TIME = LocalTime.of(9, 0);   // 09:00
+    private static final LocalTime DEFAULT_OPERATION_END_TIME = LocalTime.of(18, 0);   // 18:00
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
