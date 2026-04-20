0291f30cdd2806f8baf4e5e9bb5208cdf6ae96bb
sorbet0404 <151494073+sorbet0404@users.noreply.github.com>
2026-04-20 17:14:34 +0900

    Cus-0304 TDD (#117)
    * refactor : 자동 입차와 자동 출자 수정
    
    * refactor: 모든 코드에 흩어져 있는 예외를 GlobalExceptionHandler로 모았음
    
    * refactor : 모든 controller RsData로 감쌈
    
    * refactor : 프론트에서 결제할수 있도록 예약 DTO 구조 변경
    
    * refactor :  SSE 없는 메소드에 SSE 추가
    
    * refactor : SSE 빠진 부분 보강
    
    * refactor : 도커 탑재 및 설정 파일 수정
    
    * refator : 회원가입과 로그인 안되는 문제 해결
    
    * refactor : 고객과 관리자를 구분할수 있도록 UserProfileResDto 수정
    
    * chore : swagger문서 적용
    
    * test : 예약 서비스 테스트 코드 작성

--- src/main/java/com/example/parking/domain/user/service/UserService.java
+++ src/main/java/com/example/parking/domain/user/service/UserService.java
@@ -10,6 +10,8 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.regex.Pattern;
+
 @Service
 @RequiredArgsConstructor
 @Transactional(readOnly = true)
